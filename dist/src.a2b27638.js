41ca0ef0213caf829c3158ab6b923c6266e04fad
sakusan <sssaku.039@gmail.com>
2023-05-26 11:42:58 +0000

    Update 5 files未完了のリストに追加まで

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -176,6 +176,28 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./styles.css");
+// テキストボックスの値を取得し、初期化する
+var onClickAdd = function onClickAdd() {
+  var inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  var div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  var li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタブの子要素に各要素を設定
+  div.appendChild(li);
+
+  // 未完了のリストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+document.getElementById("add-button").addEventListener("click", function () {
+  return onClickAdd();
+});
 },{"./styles.css":"src/styles.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -201,7 +223,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45157" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46419" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
